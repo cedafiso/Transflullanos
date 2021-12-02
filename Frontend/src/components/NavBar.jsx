d7bdc2ea6208245da8582114a1b96790693019bf
cedafiso <cedafiso@gmail.com>
2021-12-01 18:43:16 -0800

    Again

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -15,19 +15,11 @@ const NavBar = () => {
 
       <nav id="navbar" className="navbar">
         <ul>
-<<<<<<< HEAD
           <li><Link to="/" className="nav-link">Home</Link></li>
           <li><Link to="/products" className="nav-link">Productos</Link></li>
           <li><Link to="/nosotros" className="nav-link">Nosotros</Link></li>
           <li><Link to="/contactos" className="nav-link">Contacto</Link></li>
           <li><Link to="/login" className="getstarted">Iniciar Sesión</Link></li>
-=======
-          <li><Link to="/" class="active">Home</Link></li>
-          <li><Link to="/products">Productos</Link></li>
-          <li><Link to="/nosotros">Nosotros</Link></li>
-          <li><Link to="/contactos">Contacto</Link></li>
-          <li><Link to="/register" class="getstarted">Iniciar Sesión</Link></li>
->>>>>>> 5bf3b085f043d8263d98b71ee74a413f30184a2d
         </ul>
         <i className="bi bi-list mobile-nav-toggle"></i>
       </nav>
