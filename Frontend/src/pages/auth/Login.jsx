69075ef83df51c694fb8bc4e70cb0691281f89e7
cedafiso <cedafiso@gmail.com>
2021-12-01 17:55:58 -0700

    FINALLY FIX

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import NavBar from "../../components/NavBar";
 import LoginForm from "../../components/FormLogin";
-=======
-import { Link } from "react-router-dom";
-import image from "./img/.png"
->>>>>>> 5bf3b085f043d8263d98b71ee74a413f30184a2d
 
 const Login = () => {
 	return ( 
