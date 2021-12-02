112438884f40a1cab801faa504c35f58107ffe8f
Daniela Bedoya Poveda <danielapoveda75@gmail.com>
2021-12-02 16:14:37 -0500

    Mejoras en las imagenes login y register

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -1,13 +1,51 @@
-import NavBar from "../../components/NavBar";
-import LoginForm from "../../components/FormLogin";
+import React from "react";
+import { Link } from "react-router-dom";
+import image from "./img/logintwo.svg";
+import logoR from "./img/logo.png";
 
-const Login = () => {
-	return ( 
-		<div>
-			<NavBar />
-			<LoginForm />
-		</div>
-	 );
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ username: "", password: "" });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="content-all">
+        <div className="ctn-form">
+          <img src={logoR} alt="Logo ferremax" className="logo" />
+          <h1 className="tittle">INICIAR SESIÓN</h1>
+          <p>Ingrese al sistema si ya está registrado.</p>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="">Correo electrónico</label>
+            <input type="email" required />
+            <span className="msg-error"></span>
+            <label htmlFor="">Contraseña</label>
+            <input type="password" required />
+            <span className="msg-error"></span>
+            <input type="submit" value="Iniciar Sesión" />
+          </form>
+
+          <span className="text-footer">
+            ¿Aún no te has registrado? <Link to="/register">Regístrate</Link>
+          </span>
+        </div>
+
+        <div className="ctn-image">
+          <img className="image" src={image} alt="" />
+        </div>
+      </div>
+    );
+  }
 }
- 
-export default Login;
