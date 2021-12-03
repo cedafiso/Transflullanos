044f37892fea69ba85b2967827cb80e2d81efb78
Jovanssj <jovanp@uninorte.edu.co>
2021-12-03 09:35:47 -0500

    servicio
    adelanto de servicios

--- Frontend/src/components/FormProducts.jsx
+++ Frontend/src/components/FormProducts.jsx
@@ -4,89 +4,76 @@ import React from "react";
 export default class ProductosForm extends React.Component {
     render() {
         return (
-            <div class="cont_principal">
-	
-            <div class="gallery js-flickity" data-flickity-options='{ "prevNextButtons": false,  "pageDots": false  }'
-            >
-           
-              <div class="gallery-cell">
-                  <div class="cont_libro_sup">   </div>
-                  <div class="cont_libro_back" >  
-            <div class="cont_detalles_lib">
-            <h4>UNDERSTANDING INDUSTRIAL DESIGN</h4>	
-            <p>The Art of Budding great user Experience in Sofware</p>
-             
-            <h3 class="precio">$28.39</h3>
-            <p class="sub_precio"><s>$44.99</s></p>
+  
+<div class="container">
+    <h1>Service box</h1>
+    <div class="row">
+        <div class="col-md-3 col-sm-6 ">
+            <div class="service-box">
+                <div class="service-icon yellow">
+                    <div class="front-content">
+                        <i class="fa fa-trophy"></i>
+                        <h4>Servicios de envío</h4>
+                    </div>
+                </div>
+                <div class="service-content">
+                    <h3>Servicios de envío</h3>
+                    <p>Con Nuestro servicio podras hacer tu envío terrestre de manera instantánea, planificarlo y seguirlo en todo momento.</p>
+                </div>
             </div>
-            
-            <div class="cont_btn_buy"> <a href="#">BUY NOW</a></div>
+        </div>
+        <div class="col-md-3 col-sm-6 ">
+            <div class="service-box">
+                <div class="service-icon orange">
+                    <div class="front-content">
+                        <i class="fa fa-anchor"></i>
+                        <h4>SEGURIDAD</h4>
+                    </div>
+                </div>
+                <div class="service-content">
+                    <h3>SEGURIDAD</h3>
+                    <p>Tu mercancía siempre esta 100% segura y protegida de toda eventualidad.</p>
+                </div>
             </div>
-              </div>
-           
-            
-            
-              <div class="gallery-cell">
-                  <div class="cont_libro_sup">   </div>
-                  <div class="cont_libro_back" >  
-            <div class="cont_detalles_lib">
-            <h4>EFFECTIVE UI</h4>	
-            <p>The Art of Budding great user Experience in Sofware</p>
-            <br/> 
-            <h3 class="precio">$28.39</h3>
-            <p class="sub_precio"><s>$44.99</s></p>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="service-box ">
+                <div class="service-icon red">
+                    <div class="front-content">
+                        <i class="fa fa-trophy"></i>
+                        <h4>MONITOREO</h4>
+                    </div>
+                </div>
+                <div class="service-content">
+                    <h3>MONITOREO</h3>
+                    <p>Monitorea en tiempo real toda la gestión de despachos.</p>
+                </div>
             </div>
-            
-            <div class="cont_btn_buy"> <a href="#">BUY NOW</a></div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="service-box">
+                <div class="service-icon grey">
+                    <div class="front-content">
+                        <i class="fa fa-paper-plane-o"></i>
+                        <h3>PAGOS</h3>
+                    </div>
+                </div>
+                <div class="service-content">
+                    <h3>jPAGOS</h3>
+                    <p>Pagas por lo que realmente usas, espacio, alistamiento y despacho del pedido.</p>
+                </div>
             </div>
-              </div>
+        </div>
+    </div>
+</div>
+         
+         
+
             
-            
-            
-            
-              <div class="gallery-cell">
-                  <div class="cont_libro_sup">  </div>
-                  <div class="cont_libro_back" >  
-            <div class="cont_detalles_lib">
-            <h4>DESIGNING SOCIAL INTERFACES</h4>	
-            <p>The Art of Budding great user Experience in Sofware</p>
-             <h3 class="precio">$28.39</h3>
-            <p class="sub_precio"><s>$44.99</s></p>
-            </div>
-            
-            <div class="cont_btn_buy"> <a href="#">BUY NOW</a></div>
-            </div>
-              </div>
-            
-            
-            
-            
-              <div class="gallery-cell">
-                  <div class="cont_libro_sup">   </div>
-                  <div class="cont_libro_back" >  
-            <div class="cont_detalles_lib">
-            <h4>DESIGNING A UX PORTFOLIO</h4>	
-            <p>The Art of Budding great user Experience in Sofware</p>
-            <br/>
-            <h3 class="precio">$28.39</h3>
-            <p class="sub_precio"><s>$44.99</s></p>
-            </div>
-            
-            <div class="cont_btn_buy"> <a href="#">BUY NOW</a></div>
-            </div>
-              </div>
-            
-            
-             
-             
-            </div>
-            
-                
-            </div> 
          )
      }
 }
 
 
- 
+
 
