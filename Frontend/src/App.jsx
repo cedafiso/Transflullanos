bb48d1bb155738557e32b2ad6128352a0dc0e7b1
Daniela Bedoya Poveda <luisballesteros96@hotmail.com>
2021-12-03 23:08:55 -0500

    Dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,29 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/home/Home"
-import Products from "./pages/products/Products"
-import Nosotros from "./pages/nosotros/Nosotros"
-import Contactos from "./pages/contactos/Contactos"
-import Login from "./pages/auth/Login"
-import Register from "./pages/auth/Register"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Nosotros from "./pages/nosotros/Nosotros";
+import Contactos from "./pages/contactos/Contactos";
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Register from "./pages/auth/Register";
+import UserInterno from "./pages/dashboard/UserInterno";
 
 function App() {
   return (
-   <BrowserRouter>
-     <Routes>
-     <Route exact path="/"element={<Home/>}/>
-     <Route exact path="/products"element={<Products/>}/>
-     <Route exact path="/nosotros"element={<Nosotros/>}/>
-     <Route exact path="/contactos"element={<Contactos/>}/>
-     <Route exact path="/login"element={<Login/>}/>
-     <Route exact path="/register"element={<Register/>}/>
-     </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <div id="content-wrapper">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/nosotros" element={<Nosotros />} />
+          <Route exact path="/contactos" element={<Contactos />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/userinterno" element={<UserInterno />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend/src/pages/dashboard/UserInterno.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+
+const UserInterno = () => {
+  return (
+    <React.Fragment>
+      <div id="wrapper">
+        {/* <!-- Sidebar --> */}
+        <Sidebar />
+        {/* <!-- End of Sidebar --> */}
+
+        {/* <!-- Content Wrapper --> */}
+        <div id="content-wrapper" className="d-flex flex-column">
+          {/* <!-- Main Content --> */}
+          <div id="content">
+            {/* <!-- Topbar --> */}
+            <Topbar />
+            {/* <!-- End of Topbar --> */}
+
+            {/* <!-- Begin Page Content --> */}
+            <div className="container-fluid">
+              {/* <!-- Page Heading --> */}
+              <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 className="h3 mb-0 text-gray-800">Usuario interno</h1>
+              </div>
+
+              {/* Content page */}
+
+              {/* <!-- Content Row --> */}
+            </div>
+            {/* <!-- /.container-fluid --> */}
+          </div>
+          {/* <!-- End of Main Content --> */}
+        </div>
+        {/* <!-- End of Content Wrapper --> */}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default UserInterno;
